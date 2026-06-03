d6503834f8d303147d626c5829a5865ad18d3b6d
Tassel <2656142+tasselx@users.noreply.github.com>
2026-06-03 18:17:53 +0800

    Update Keyden to version 1.0.10

--- Casks/keyden.rb
+++ Casks/keyden.rb
@@ -1,9 +1,9 @@
 cask "keyden" do
-  version "1.0.9"
+  version "1.0.10"
   arch arm: "arm64", intel: "x86_64"
 
-  sha256 arm:   "fe17a902d2e44edbfe5d73b473e68e749b8d957a4397cfdddd8cec647e7af667",
-         intel: "4f25f939629ebc19c6c81a663f512149e563ae603398b14178ea7f9a2a5a53d0"
+  sha256 arm:   "2f46603aa28168f18df0b45dd76461a7791ccabffa3ccc61a62595b94a7b21da",
+         intel: "1f119cfa05ccacd9f8255ecc22ae7d2843b6bf09888eb9ea02ac613fb45c68f5"
 
   url "https://github.com/tasselx/Keyden/releases/download/v#{version}/Keyden-#{version}-#{arch}.dmg",
       verified: "github.com/tasselx/Keyden/"
